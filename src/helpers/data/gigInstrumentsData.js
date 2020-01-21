2937a502726a9c8d876c6f26d2fe6637f5ed753d
Denise Baker <denviol@yahoo.com>
2020-01-21 13:46:44 -0600

    added gigInstrument data

---
+++ src/helpers/data/gigInstrumentsData.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAllGigInstruments = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/gigInstruments.json`)
+    .then((result) => {
+      const gigInstrumentObject = result.data;
+      const gigInstruments = [];
+      if (gigInstrumentObject != null) {
+        Object.keys(gigInstrumentObject).forEach((fbId) => {
+          gigInstrumentObject[fbId].id = fbId;
+          gigInstruments.push(gigInstrumentObject[fbId]);
+        });
+      }
+      resolve(gigInstruments);
+    })
+    .catch((error) => reject(error));
+});
+
+const deleteGigInstrument = (gigInstrumentId) => axios.delete(`${baseUrl}/gigInstruments/${gigInstrumentId}.json`);
+const addGigInstrument = (newGigInstrument) => axios.post(`${baseUrl}/gigInstruments.json`, newGigInstrument);
+const updateGigInstrument = (gigInstrumentId, newGigInstrumentInfo) => axios.put(`${baseUrl}/gigInstruments/${gigInstrumentId}.json`, newGigInstrumentInfo);
+
+export default {
+  getAllGigInstruments,
+  addGigInstrument,
+  updateGigInstrument,
+  deleteGigInstrument,
+};

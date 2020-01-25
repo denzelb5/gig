a4c4d2911d36445a9aa652ffe9a247950e3bab24
Denise Baker <denviol@yahoo.com>
2020-01-24 18:47:23 -0600

    gig add works need to add instruments

--- src/components/shared/GigCard/GigCard.js
+++ src/components/shared/GigCard/GigCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import gigShape from '../../../helpers/propz/gigShape';
+// import PropTypes from 'prop-types';
+// import gigShape from '../../../helpers/propz/gigShape';
 
 
 import './GigCard.scss';

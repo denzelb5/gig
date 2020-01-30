2b5fcc6e105999f9a6ae29dca8738abb77509194
Denise Baker <denviol@yahoo.com>
2020-01-29 22:09:18 -0600

    roster functionality mostly working

--- src/components/pages/Roster/Roster.js
+++ src/components/pages/Roster/Roster.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import InstrumentMenu from '../../shared/InstrumentMenu/InstrumentMenu';
-import instrumentData from '../../../helpers/data/instrumentData';
-import playerData from '../../../helpers/data/playerData';
-
+// import InstrumentMenu from '../../shared/InstrumentMenu/InstrumentMenu';
 import PlayerRow from '../../shared/PlayerRow/PlayerRow';
+
+import smash from '../../../helpers/data/smash';
 import './Roster.scss';
 
 class Roster extends React.Component {
@@ -14,36 +13,38 @@ class Roster extends React.Component {
   }
 
   state = {
-    players: [],
-    instrumentNames: [],
+    instruments: [],
   }
 
   getInstruments = () => {
-    instrumentData.getAllInstruments()
-      .then((instrumentNames) => this.setState({ instrumentNames }))
-      .catch((error) => console.error(error));
-  }
-
-  getPlayers = () => {
-    playerData.getAllPlayers()
-      .then((players) => this.setState({ players }))
+    const { gigId } = this.props.match.params;
+    smash.getInstrumentPlayersForRoster(gigId)
+      .then((instruments) => this.setState({ instruments }))
       .catch((error) => console.error(error));
   }
 
   componentDidMount() {
-    this.getPlayers();
     this.getInstruments();
   }
 
   render() {
     const { gigId } = this.props.match.params;
+    const { instruments } = this.state;
+    const printRoster = () => instruments.map((instrument) => (
+      <div>
+        <h1>{instrument.name}</h1>
+        <h2>Number: {instrument.number}</h2>
+        {instrument.players.map((player) => <PlayerRow key={player.id} player={player} />)}
+      </div>
+    ));
+
     return (
       <div className="roster">
       <h1>Roster page</h1>
       <Link className="btn btn-success" to="/gig/:gigId/edit">to gig edit</Link>
       <Link className="btn btn-success" to={`/gig/:${gigId}`}>View Gig</Link>
-      {this.state.players.map((player) => <PlayerRow key={player.id} player={player} />)}
-      {this.state.instrumentNames.map((instrument) => <InstrumentMenu key={instrument.id} instrument={instrument} />)}
+      {printRoster()}
+      {/* {this.state.instrumentNames.map((instrument) => <InstrumentMenu key={instrument.id} instrument={instrument} />)} */}
       </div>
     );
   }
---
+++ src/helpers/data/gigPlayerData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAllGigPlayersByGigId = (gigId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/gigPlayers.json?orderBy="gigId"&equalTo="${gigId}"`)
+    .then((result) => {
+      const gigPlayers = result.data;
+      const final = [];
+      if (gigPlayers != null) {
+        Object.keys(gigPlayers).forEach((fbId) => {
+          gigPlayers[fbId].id = fbId;
+          final.push(gigPlayers[fbId]);
+        });
+      }
+      resolve(final);
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getAllGigPlayersByGigId };
